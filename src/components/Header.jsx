d6b936b8534043d04c155692e1d6f3e5d6b717e5
Anicet Chiza <chizaanicet@gmail.com>
2025-10-21 10:59:16 +0200

    this is a real estate website that i am building in charge of learning React JS

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {assets} from '../assets/assets';
+import Navbar from './Navbar';
+
+const Header = () => {
+    return (
+        <div className='min-h-screen bg-cover bg-center flex items-center justify-center w-full overflow-hidden' style={{backgroundImage: `url(${assets.header_img})`}}>
+            <Navbar />
+
+            {/* Hero section */}
+            <div className='container text-white px-4 flex flex-col justify-center items-center gap-4 sm:px-12 lg:px-24 xl:px-40 text-center'>
+                <h2 className='text-5xl sm:text-[40px] md:text-[70px] max-w-3xl font-bold'>Explore Homes That Fit <span className='bg-gradient-to-r from-[#38BDF8] to-[#fcd3a4] bg-clip-text text-transparent'>Your Dreams.</span></h2>
+                <div className='flex items-center gap-4 mt-10'>
+                    <a className='border border-white px-8 py-3 rounded' href="#">Our Projects</a>
+                    <a className='bg-[#38BDF8] border border-[#38BDF8] px-8 py-3 rounded' href="#">Contact Us</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {assets} from '../assets/assets';
+
+const Navbar = () => {
+    return (
+        <div className='absolute top-0 w-full z-9 border-b border-gray-600'>
+            <div className='flex items-center justify-between py-6 px-4 sm:px-12 lg:px-24 xl:px-40'>
+                <img src={assets.logo} alt="" />
+                <ul className='hidden md:flex gap-6'>
+                    <li><a className='text-white/90 hover:text-sky-400' href="#header">Home</a></li>
+                    <li><a className='text-white/90 hover:text-sky-400' href="#about">About</a></li>
+                    <li><a className='text-white/90 hover:text-sky-400' href="#projects">Projects</a></li>
+                    <li><a className='text-white/90 hover:text-sky-400' href="#testimonials">Testimonials</a></li>
+                </ul>
+                <button className='hidden md:flex bg-sky-400 text-white px-8 py-2 rounded'>Sign up</button>
+
+                {/* Menu icon */}
+                <div className='md:hidden flex border border-gray-600 px-3 py-3 rounded'>
+                    <img src={assets.menu_icon} className='w-5 cursor-pointer' alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
