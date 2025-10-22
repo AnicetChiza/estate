78d0e71f3c62881ba9f782a8da909915e405355d
Anicet Chiza <chizaanicet@gmail.com>
2025-10-22 12:07:56 +0200

    add to my repository

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='flex flex-col py-6 px-4 sm:px-12 lg:px-24 xl:px-40'>
+            <h1>About <span>Our Brand</span></h1>
+            <p>Passionate About Properties, Dedicated to Your Vision</p>
+        </div>
+    )
+}
+
+export default About
