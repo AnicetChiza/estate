cd9e4e249076768ab733bb281e98486db9030d20
Anicet Chiza <chizaanicet@gmail.com>
2025-10-22 14:13:15 +0200

    add this page to my repository

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <div id='projects' className='container flex flex-col items-center  py-6 px-4 sm:px-12 lg:px-24 xl:px-40'>
+            <h2 className='text-[25px] sm:text-[30px] md:text-[40px] font-bold'>Projects <span className='bg-gradient-to-r from-[#38BDF8] to-[#fcd3a4] bg-clip-text text-transparent'>Completed</span></h2>
+            <p className='text-gray-400 max-w-80'>Crafting Space, Building legacies-Explore Our Portfolio.</p>
+        </div>
+    )
+}
+
+export default Projects
