bc16dd9fdd476621541eb94eb6dbcd7051d21404
Anicet Chiza <chizaanicet@gmail.com>
2025-10-22 11:41:36 +0200

    add to my repository

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useState } from 'react';
 import {assets} from '../assets/assets';
 
 const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-        <div className='absolute top-0 w-full z-9 border-b border-gray-600'>
+        <div className='absolute top-0 w-full z-9'>
             <div className='flex items-center justify-between py-6 px-4 sm:px-12 lg:px-24 xl:px-40'>
                 <img src={assets.logo} alt="" />
                 <ul className='hidden md:flex gap-6'>
@@ -15,13 +19,14 @@ const Navbar = () => {
                 <button className='hidden md:flex bg-sky-400 text-white px-8 py-2 rounded'>Sign up</button>
 
                 {/* Menu icon */}
-                <div className='md:hidden flex border border-gray-600 px-3 py-3 rounded'>
+                <div onClick={() => setMenuOpen(true)} className='md:hidden flex border border-gray-600 px-3 py-3 rounded'>
                     <img src={assets.menu_icon} className='w-5 cursor-pointer' alt="" />
                 </div>
+            </div>
 
-                {/* Menu for mobile screen */}
-                <div className='fixed top-0 left-0 bottom-0 right-0 w-full px-12 py-6 bg-gray-800/90 transition-all duration-300'>
-                    <div className='flex justify-end cursor-pointer border border-gray-600 px-3 py-3 rounded w-fit ml-auto'>
+            {/* Menu for mobile screen */}
+                <div className={`md:hidden ${menuOpen ? 'fixed w-full' : 'w-0 h-0 opacity-0 invisible'} top-0 left-0 bottom-0 right-0 overflow-hidden px-12 py-6 bg-gray-800/90 backdrop-blur-sm transition-all duration-300`}>
+                    <div onClick={() => setMenuOpen(false)} className='flex justify-end cursor-pointer border border-gray-600 px-3 py-3 rounded w-fit ml-auto'>
                         <img src={assets.cross_icon} className='w-4 invert brightness-100' alt="" />
                     </div>
                     <ul className='flex flex-col gap-7'>
@@ -31,7 +36,6 @@ const Navbar = () => {
                         <a className='text-white/90 text-[17px] w-fit hover:text-sky-400' href="#testimonials">Testimonials</a>
                     </ul>
                 </div>
-            </div>
         </div>
     )
 }
