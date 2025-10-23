7ccca5ec1f9655ee787d03a558600a7880979fe2
Anicet Chiza <chizaanicet@gmail.com>
2025-10-23 15:32:41 +0200

    add the additionnal things to my projects

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
     }
 
     return (
-        <div className='py-15 px-4 sm:px-12 lg:px-24 xl:px-40'>
+        <div id='contact' className='py-15 px-4 sm:px-12 lg:px-24 xl:px-40'>
             <div className='flex flex-col items-center justify-center'>
                 <h1 className='text-[25px] text-gray-700 sm:text-[30px] md:text-[40px] font-bold'>Contact <span className='bg-gradient-to-r from-[#38BDF8] to-[#fcd3a4] bg-clip-text text-transparent'>With Us</span></h1>
                 <p className='mt-4 max-w-80 text-center text-gray-400'>Ready to make a move? Let's build your future together.</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,8 +11,8 @@ const Header = () => {
             <div className='container text-white px-4 flex flex-col justify-center items-center gap-4 sm:px-12 lg:px-24 xl:px-40 text-center'>
                 <h2 className='text-4xl text-[35px] sm:text-[40px] md:text-[70px] max-w-3xl font-bold'>Explore Homes That Fit <span className='bg-gradient-to-r from-[#38BDF8] to-[#fcd3a4] bg-clip-text text-transparent'>Your Dreams.</span></h2>
                 <div className='flex items-center gap-4 mt-10'>
-                    <a className='border border-white px-8 py-3 rounded' href="#">Our Projects</a>
-                    <a className='bg-[#38BDF8] border border-[#38BDF8] px-8 py-3 rounded' href="#">Contact Us</a>
+                    <a className='border border-white px-8 py-3 rounded' href="#projects">Our Projects</a>
+                    <a className='bg-[#38BDF8] border border-[#38BDF8] px-8 py-3 rounded' href="#contact">Contact Us</a>
                 </div>
             </div>
         </div>
